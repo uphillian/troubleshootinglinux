6b0c9210f71d6a4927e389d6017a518495a58e22
Thomas Uphill <thomas@narrabilis.com>
2016-04-13 00:46:46 -0400

    fixed up signalling

--- zombie.py
+++ zombie.py
@@ -3,14 +3,19 @@
 import os
 import sys
 import time
+import signal
 
+def sigcont(signum, frame):
+  print("Received %d" % signum)
+
+signal.signal(signal.SIGCONT, sigcont)
 pid = os.fork()
 if pid == 0:
-  print("Child")
+  print("Child exiting")
   sys.exit(0)
 else:
-  print("Parent, Child PID: %s\nWaiting for Input" % pid)
-  input = sys.stdin.readlines()
+  print("Parent PID %s, Child PID: %s\nWaiting for Signal" % (os.getpid(),pid) )
+  signal.pause()
   os.wait()
   print("Zombie is gone\n") 
   sys.exit(0)
