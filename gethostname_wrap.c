3b22205da2c7f598fdb9f00e6322922b7fa3a739
Thomas Uphill <thomas@narrabilis.com>
2019-09-30 11:39:52 -0700

    override the gethostname function

---
+++ gethostname_wrap.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int gethostname (char *name, size_t len) {
+  char newname[] = "getip_hostname";
+  int name_len = strlen(newname);
+  memcpy(name,newname, name_len < len ? name_len : len);
+  return 0;
+}
