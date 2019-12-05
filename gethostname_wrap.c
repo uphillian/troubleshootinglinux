5b2bd2a5627c81965e5f34cf43ca7a5f9cfb038f
Thomas Uphill <thomas@narrabilis.com>
2019-12-05 08:01:39 -0800

    null termination on the string

--- gethostname_wrap.c
+++ gethostname_wrap.c
@@ -5,7 +5,7 @@
 
 int gethostname (char *name, size_t len) {
   char newname[] = "getip_hostname";
-  int name_len = strlen(newname);
+  int name_len = strlen(newname) + 1;
   memcpy(name,newname, name_len < len ? name_len : len);
   return 0;
 }
